b6079c46cb687592ac6d48a67e37b5dd34cbfa4b
zakrnem <zakrnem@zohomail.com>
2023-08-14 20:51:31 -0600

    Personal details component initial layout

---
+++ src/components/PersonalDetails.jsx
@@ -0,0 +1,29 @@
+import { useState } from "react"
+import '../styles/PersonalDetails.css'
+
+function PersonalDetails() {
+    const [input, setInput] = useState([])
+    function handleChange() {
+        console.log(input)
+    }
+    return (
+        <div className="personal-details">
+            <div className="personal-heading">
+                <h2>Personal Details</h2>
+                <button><img src="./src/assets/down-svgrepo-com.svg" /></button>
+            </div>
+            <form>
+                <label htmlFor="fullName">Full name</label>
+                <input type="text" value={input.name} onChange={handleChange} />
+                <label htmlFor="email">Email</label>
+                <input type="email" value={input.email} onChange={handleChange} />
+                <label htmlFor="phoneNumber">Phone number</label>
+                <input type="tel" value={input.number} onChange={handleChange} />
+                <label htmlFor="address">Address</label>
+                <input type="text" value={input.address} onChange={handleChange} />
+            </form>
+        </div>
+    )
+}
+
+export default PersonalDetails
