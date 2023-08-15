636af2e5cdb58bcdec35d083b6f092d4185ba9b3
zakrnem <zakrnem@zohomail.com>
2023-08-14 21:06:21 -0600

    Style main page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import './App.css'
+import PersonalDetails from './components/PersonalDetails'
 
 function App() {
 
   return (
     <>
-      <h1>CV Builder</h1>
+      <header>
+        <img src="./src/assets/square-cv-svgrepo-com.svg" className='title-logo'/>
+        <h1>CV Builder</h1>
+      </header>
+      <PersonalDetails />
     </>
   )
 }
