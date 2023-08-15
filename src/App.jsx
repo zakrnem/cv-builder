834a56e5b7c2260d157f6195f8579938cf9c6a24
zakrnem <zakrnem@zohomail.com>
2023-08-14 21:35:48 -0600

    Separate header into separate component
    Header changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
 import './App.css'
+import Header from './components/Header'
 import PersonalDetails from './components/PersonalDetails'
 
 function App() {
 
   return (
     <>
-      <header>
-        <img src="./src/assets/square-cv-svgrepo-com.svg" className='title-logo'/>
-        <h1>CV Builder</h1>
-      </header>
+      <Header />
       <PersonalDetails />
     </>
   )
