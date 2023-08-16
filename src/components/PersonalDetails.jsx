519cedd78d63290f37f1e386fcd2bc06441e67ab
zakrnem <zakrnem@zohomail.com>
2023-08-15 20:30:29 -0600

    Personal details: toggle form button working

--- src/components/PersonalDetails.jsx
+++ src/components/PersonalDetails.jsx
@@ -3,20 +3,28 @@ import '../styles/PersonalDetails.css'
 
 function PersonalDetails() {
     const [input, setInput] = useState({name: '', email: '', number: '', address: ''})
+    const [isFormVisible, setIsFormVisible] = useState(true);
+
     const handleInputChange = (field, value) => {
         setInput(prevInput => ({
             ...prevInput,
             [field]: value
         }));
         console.log(input)
-    };
+    }
+    const toggleForm = () => {
+        setIsFormVisible(prevIsFormVisible => !prevIsFormVisible);
+      };
     return (
         <div className="personal-details">
             <div className="personal-heading">
                 <h2>Personal Details</h2>
-                <button><img src="./src/assets/down-svgrepo-com.svg" /></button>
+                <button onClick={toggleForm} style={{transition: 'transform 0.3s', 
+                transform: isFormVisible ? '' : 'rotate(180deg)'}}>
+                    <img src="./src/assets/down-svgrepo-com.svg" />
+                </button>
             </div>
-            <form>
+            <form style={{display: isFormVisible ? '' : 'none'}}>
                 <label htmlFor="fullName">Full name</label>
                 <input type="text" value={input.name} onChange={(e) => handleInputChange('name', e.target.value)} />
                 <label htmlFor="email">Email</label>
