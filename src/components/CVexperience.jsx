7d62731c7020381f77f77115adbc453778e500f3
zakrnem <zakrnem@zohomail.com>
2023-08-19 11:54:20 -0600

    Create CVexperience component

--- src/components/CVexperience.jsx
+++ src/components/CVexperience.jsx
@@ -1,7 +1,23 @@
-function CVexperience({}) {
-    return (
-        <div className="cv-experience">
-
-        </div>
-    )
+function CVexperience({
+  company,
+  position,
+  startDate,
+  endDate,
+  location,
+  description,
+}) {
+  return (
+    <div className="cv-experience">
+      <h2>Professional Experience</h2>
+      <div className="cv-experience-grid">
+        <div>Date</div>
+        <div>Location</div>
+        <div>Company Name</div>
+        <div>Job Title</div>
+        <div>Description</div>
+      </div>
+    </div>
+  );
 }
+
+export default CVexperience;
