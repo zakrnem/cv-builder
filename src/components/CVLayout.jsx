46dea5d75adf51733ed31bc03e5e431e60cea56f
zakrnem <zakrnem@zohomail.com>
2023-08-18 19:14:25 -0600

    Refine stylesheet to achieve the intended layout

--- src/components/CVLayout.jsx
+++ src/components/CVLayout.jsx
@@ -6,17 +6,21 @@ function CVLayout() {
       <div className="cv-personal-details">
         <h2>Full name</h2>
         <div className="cv-details">
-          <img src="./src/assets/mail-svgrepo-com.svg" className="icon" />
-          <div className="email">Email</div>
-
-          <img src="./src/assets/phone-svgrepo-com.svg" className="icon" />
-          <div className="phone">+123 0000 0000</div>
-
-          <img
-            src="./src/assets/location-pin-svgrepo-com.svg"
-            className="icon"
-          />
-          <div className="location">Location</div>
+          <div className="email">
+            <img src="./src/assets/mail-svgrepo-com.svg" className="icon" />
+            Email
+          </div>
+          <div className="phone">
+            <img src="./src/assets/phone-svgrepo-com.svg" className="icon" />
+            +123 0000 0000
+          </div>
+          <div className="location">
+            <img
+              src="./src/assets/location-pin-svgrepo-com.svg"
+              className="icon"
+            />
+            Location
+          </div>
         </div>
       </div>
     </div>
